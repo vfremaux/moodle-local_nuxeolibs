6aea692d9732ca918e88ef6e4e0e0f7a37c12c85
valery <valery.fremaux@gmail.com>
2017-06-11 14:22:18 +0200

    code load

---
+++ version.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details.
+ *
+ * @package    local_nuxeolibs
+ * @category   local
+ * @copyright  Académie de Rennes (http://www.toutatice.fr)
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version  = 2016052300;   // The (date) version of this plugin.
+$plugin->requires = 2016051900;   // Requires this Moodle version.
+$plugin->release = '3.1.0 (Build 2015100600)';
+$plugin->component = 'local_nuxeolibs';
+$plugin->maturity = MATURITY_RC;
+
+// Non moodle attributes.
+$plugin->codeincrement = '3.1.0000';
