ed2fe8738d281bf19cb7253c936548fd31e7e0e7
valery <valery.fremaux@gmail.com>
2018-01-31 14:50:03 +0100

    Add missing branches

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2016052300;   // The (date) version of this plugin.
-$plugin->requires = 2016051900;   // Requires this Moodle version.
-$plugin->release = '3.1.0 (Build 2015100600)';
+$plugin->requires = 2016120500;   // Requires this Moodle version.
+$plugin->release = '3.2.0 (Build 2015100600)';
 $plugin->component = 'local_nuxeolibs';
 $plugin->maturity = MATURITY_RC;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.1.0000';
+$plugin->codeincrement = '3.2.0000';
