ff3a3db6d9524a7a18093351bbc30ea141d2e73e
valery <valery.fremaux@gmail.com>
2020-04-14 15:31:09 +0200

    Resync all locals

--- version.php
+++ version.php
@@ -26,10 +26,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version  = 2016052300;   // The (date) version of this plugin.
-$plugin->requires = 2018042700;   // Requires this Moodle version.
-$plugin->release = '3.5.0 (Build 2015100600)';
+$plugin->requires = 2018112800;   // Requires this Moodle version.
+$plugin->release = '3.6.0 (Build 2015100600)';
 $plugin->component = 'local_nuxeolibs';
 $plugin->maturity = MATURITY_RC;
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.5.0000';
+$plugin->codeincrement = '3.6.0000';
